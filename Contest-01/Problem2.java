507260fc64ed32f7f660516501bec6f3b7f11f58
Rajeev Kumar <rajeevjewar2@gmail.com>
2023-09-23 01:30:44 +0530

    Problem 1, 2, 3, 4 is solved

---
+++ Contest-01/Problem2.java
@@ -0,0 +1,13 @@
+public class Problem2 {
+  public static void main(String[] args) {
+    // final int a = 10, b = 20;
+    // It will not work with final keyword because final keyword makes value of an
+    // integer fixed, we can not change the value of variable once we final the
+    // variable.
+    int a = 10, b = 20;
+    while (a > b) {
+      System.out.println("Hello");
+    }
+    System.out.println("GEEKS");
+  }
+}
