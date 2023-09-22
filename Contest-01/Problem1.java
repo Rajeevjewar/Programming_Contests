0dd9174479e74f52bc3ef760686799e978f8c88d
Rajeev Kumar <rajeevjewar2@gmail.com>
2023-09-23 01:21:59 +0530

    Problem 1 is solved

---
+++ Contest-01/Problem1.java
@@ -0,0 +1,10 @@
+public class Problem1 {
+  public static void main(String[] args) {
+    int x = 10;
+    if (++x < 10 && (x / 0 > 10)) {
+      System.out.println("Ankit");
+    } else {
+      System.out.println("GEEKS");
+    }
+  }
+}
