80b1b5e44185bb63c550edd78da16b73008abd6a
Rajeev Kumar <rajeevjewar2@gmail.com>
2024-09-23 17:12:12 +0530

    Missing and Repeating is solved

--- Sept_24/src/MissingRepeating.java
+++ Sept_24/src/MissingRepeating.java
@@ -1,24 +1,27 @@
+// Missing And Repeating
+// Date: 23/09/24
+
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class MissingRepeating {
     public static void main(String[] args) {
-
+        int[] arr = {2, 2};
+        System.out.println(Arrays.toString(findTwoElement(arr)));
     }
-    int[] findTwoElement(int[] arr) {
-        // code here
+    static int[] findTwoElement(int[] arr) {
         int rep = 0, mis = 0;
         HashMap<Integer, Boolean> map = new HashMap<>();
-        for(int i = 0; i<arr.length; i++){
-            if(map.get(arr[i]) == null){
-                map.put(arr[i], true);
-            }
-            else {
-                rep = arr[i];
+        for (int i : arr) {
+            if (map.get(i) == null) {
+                map.put(i, true);
+            } else {
+                rep = i;
             }
         }
         for(int i = 1; i <= arr.length; i++){
-            if(map.get(arr[i]) == null){
-                mis = arr[i];
+            if(map.get(i) == null){
+                mis = i;
             }
         }
         return new int[]{rep, mis};
